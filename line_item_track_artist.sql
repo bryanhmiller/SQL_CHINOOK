111fcbd2f90c74cfca26359d06670fce9b1bf657
bryanhmiller <bryankalos@hotmail.com>
2017-08-10 20:44:02 -0500

    Added some alias to line_item_artist.sql

--- line_item_track_artist.sql
+++ line_item_track_artist.sql
@@ -1,4 +1,4 @@
-select il.InvoiceId, il.InvoiceLineId, a.Name, t.Name
+select il.InvoiceId, il.InvoiceLineId, a.Name 'Artist Name', t.Name 'Track Name'
 from InvoiceLine il
 	left join Track t
 		on il.TrackId = t.TrackId
